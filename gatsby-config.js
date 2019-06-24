b4a5c1246169d0814dcc1c154543978e1fccecfa
smohiudd <saadiq.mohiuddin@gmail.com>
2019-06-23 23:13:03 -0600

    change site title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   pathPrefix: "/curb-rules-map",
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Calgary Curb Rules Map`,
+    description: `Calgary Curb Rules Map`,
     author: `@gatsbyjs`,
   },
   plugins: [
